4551dd62fc97127363e1d40c50457d8eae940274
Amandine Ameye <amandineameye@gmail.com>
2024-11-14 23:38:57 +0100

    Added CarbsResultTable

--- src/containers/AddMealSteps/AddMeal2.jsx
+++ src/containers/AddMealSteps/AddMeal2.jsx
@@ -1,34 +1,39 @@
 import style from "./AddMeal2.module.css";
-import MealBCC from "../../components/MealBCC/MealBCC"
+import MealBCC from "../../components/MealBCC/MealBCC";
 import TagsChoice from "../TagsChoice/TagsChoice";
+import CarbsResultsTable from "../CarbsResultsTable/CarbsResultsTable";
 
 let page = "addMeal2";
 
-const AddMeal2 = ({onClickAddMeal}) => {
+const AddMeal2 = ({ onClickAddMeal }) => {
 	return (
-				<div className={style.mainContentDiv}>
-					<div className={style.titlesDiv}>
-						<h1>Decide on the bolus</h1>
-						<h2>Add all the precious details</h2>
-					</div>
-					<div className={style.boxesDiv}>
-						<div className={style.carbsTableDiv}></div>
-						<div className={style.bolusCalcDiv}></div>
-						<div className={style.previousMealsDiv1}>
-							<MealBCC page={page}/>
-						</div>
-						<div className={style.previousMealsDiv2}>
-						<MealBCC page={page} />
-						</div>
-						<div className={style.previousMealsDiv3}>
-						<MealBCC page={page}/>
-						</div>
-						<div className={style.tagsDiv}>
-							<TagsChoice/>
-						</div>
-					</div>
-					<button className={style.addMealButton} onClick={onClickAddMeal}>Add meal</button>
+		<div className={style.mainContentDiv}>
+			<div className={style.titlesDiv}>
+				<h1>Decide on the bolus</h1>
+				<h2>Add all the precious details</h2>
+			</div>
+			<div className={style.boxesDiv}>
+				<div className={style.carbsTableDiv}>
+					<CarbsResultsTable />
 				</div>
+				<div className={style.bolusCalcDiv}></div>
+				<div className={style.previousMealsDiv1}>
+					<MealBCC page={page} />
+				</div>
+				<div className={style.previousMealsDiv2}>
+					<MealBCC page={page} />
+				</div>
+				<div className={style.previousMealsDiv3}>
+					<MealBCC page={page} />
+				</div>
+				<div className={style.tagsDiv}>
+					<TagsChoice />
+				</div>
+			</div>
+			<button className={style.addMealButton} onClick={onClickAddMeal}>
+				Add meal
+			</button>
+		</div>
 	);
 };
 
---
+++ src/containers/CarbsResultsTable/CarbsResultsTable.jsx
@@ -0,0 +1,56 @@
+import style from "./CarbsResultsTable.module.css";
+
+const Thead = () => {
+	return (
+		<thead>
+			<tr>
+				<th>Your meal</th>
+				<th>Quantity</th>
+				<th>%</th>
+				<th>Carbs</th>
+			</tr>
+		</thead>
+	);
+};
+
+const CarbResultsRow = () => {
+	return (
+		<tr>
+			<th>White bread</th>
+			<td>130</td>
+			<td>49</td>
+			<td>65.7</td>
+		</tr>
+	);
+};
+
+const Tfoot = () => {
+	return (
+		<tfoot>
+			<tr>
+				<th>Total</th>
+				<td></td>
+				<td></td>
+				<td>
+					<p>69</p>
+				</td>
+			</tr>
+		</tfoot>
+	);
+};
+
+const CarbsResultsTable = () => {
+	return (
+		<table>
+			<Thead />
+			<tbody>
+				<CarbResultsRow />
+				<CarbResultsRow />
+				<CarbResultsRow />
+			</tbody>
+			<Tfoot />
+		</table>
+	);
+};
+
+export default CarbsResultsTable;
