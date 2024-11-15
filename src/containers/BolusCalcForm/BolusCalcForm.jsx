4fb2bc4af46237a4977aad20cebbe3123432e647
Amandine Ameye <amandineameye@gmail.com>
2024-11-15 01:05:01 +0100

    Fixed style of bolus calc form

--- src/containers/BolusCalcForm/BolusCalcForm.jsx
+++ src/containers/BolusCalcForm/BolusCalcForm.jsx
@@ -6,27 +6,36 @@ const BolusCalcForm = () => {
 			<div className={style.inputsRowsDiv}>
 				<div>
 					<label htmlFor="bloodSugarRate">Blood sugar rate</label>
-					<input type="text" name="bloodSugarRate" id="bloodSugarRate" />
-					<span>mg/dL</span>
+					<div>
+						<input type="text" name="bloodSugarRate" id="bloodSugarRate" />
+						<span>mg/dL</span>
+					</div>
 				</div>
 
 				<div>
 					<label htmlFor="correctionBolus">Correction bolus</label>
-					<input type="text" name="correctionBolus" id="correctionBolus" />
-					<span>units</span>
+					<div>
+						<input type="text" name="correctionBolus" id="correctionBolus" />
+						<span>units</span>
+					</div>
 				</div>
 
 				<div>
 					<label htmlFor="mealBolus">Meal bolus</label>
-					<input type="text" name="mealBolus" id="mealBolus" />
-					<span>units</span>
+					<div>
+						{" "}
+						<input type="text" name="mealBolus" id="mealBolus" />
+						<span>units</span>
+					</div>{" "}
 				</div>
 			</div>
 
 			<div className={style.totalBolusDiv}>
 				<p>Total bolus</p>
-				<div></div>
-				<span>units</span>
+				<div>
+					<div></div>
+					<span>units</span>
+				</div>
 			</div>
 		</form>
 	);
