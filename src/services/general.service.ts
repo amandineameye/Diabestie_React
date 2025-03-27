db9b0414ca44e3e7884c809043bcb56e8511bbe8
Amandine Ameye <amandineameye@gmail.com>
2025-03-27 23:18:45 +0100

    Used env variables to run both in local and Vercel

--- src/services/general.service.ts
+++ src/services/general.service.ts
@@ -1,12 +1,15 @@
 import axios from "axios";
 
 const apiURL = "https://diabestie-node.vercel.app/general";
+const baseURL = process.env.VITE_API_URL + '/general';
 
 
 type NamesObject = {
 	firstName: string;
 	username: string;
 };
+
+console.log(process.env.VITE_API_URL);
 
 export const fetchUserNames = async (): Promise<NamesObject> => {
 	const token = localStorage.getItem("authToken");
@@ -15,7 +18,7 @@ export const fetchUserNames = async (): Promise<NamesObject> => {
 			authorization: `Bearer ${token}`,
 		},
 	};
-	const response = await axios.get(apiURL + "/getUserNames", headers);
+	const response = await axios.get(baseURL + "/getUserNames", headers);
 	const data = response.data;
 	return data as NamesObject;
 };
